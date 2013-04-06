974f4bbcf14ae62cd128ecf1488019185c0ae4b0
Oliver Kuhn <oliver.kuhn@web.de>
2013-04-06 13:46:33 +0200

    new #defines

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define VERSION "0.01"
+#define VERSION "0.04"
 
 #define HISTSIZE 10 
 
@@ -23,12 +23,24 @@
 #define KEY2            7
 #define ALL_KEYS        (1<<KEY0 | 1<<KEY1 | 1<<KEY2)
 
+#define LIGHT_DDR  		DDRB
+#define LIGHT_PORT 		PORTB
+
+#define STATUS_DDR  	DDRC
+#define STATUS_PORT 	PORTC
+
+#define LDR 1
+#define MIN_BRIGHTNESS 30
+
+#define INVERSE 0
+
 #define BUFSIZE 40
 
 #define DEBOUNCE    256L        // debounce clock (256Hz = 4msec)
 
-#define TZ_OFFSET 3600			// winter
-// #define TZ_OFFSET 7200    	// summer
+// #define TZ_OFFSET 3600			// winter
+#define TZ_OFFSET 7200    	// summer
 
+#define LDR_ADC 0
 
 #endif // __CONFIG_H__
